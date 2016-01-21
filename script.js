946b49260c8cb4de237e987040c0dcc5491d7f5e
Ti Wegmeyer <tiwegmeyeryoga@gmail.com>
2016-01-20 17:18:21 -0700

    before end of day

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ angular.module('secondPage')
         $scope.color = "color: black";
     }
     }]);
+
+angular.module('secondPage')
+        .directive('ngConfirmClick', [
+            function(){
+                return {
+                    link: function (scope, element, attr) {
+                        var msg = attr.ngConfirmClick || "Are you sure?";
+                        var clickAction = attr.confirmedClick;
+                        element.bind('click',function (event) {
+                            if ( window.confirm(msg) ) {
+                                scope.$eval(clickAction)
+                        }
+                    
+                    });
+                }
+            };
+    }])
